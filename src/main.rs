fc57f1bbd94d31204c788dda6044bb671377f0ed
bichanna <nobu.bichanna@gmail.com>
2024-01-17 21:39:29 -0500

    Add base template

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod entities;
 
-fn main() {
-    println!("Hello, world!");
+use rocket::{self, get, launch, routes, Build, Rocket};
+
+#[get("/ping")]
+fn ping() -> &'static str {
+    "Hello, world. The site is running."
+}
+
+#[launch]
+fn launch() -> Rocket<Build> {
+    rocket::build().mount("/", routes![ping])
 }
